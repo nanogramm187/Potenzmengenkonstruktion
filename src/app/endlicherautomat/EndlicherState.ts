94b4516406fff0f4a066b6365f0899a8744a21f6
Simon Schöpke <simon2204s@gmail.com>
2024-01-30 00:49:51 +0100

    Changes for submodule dependency

--- src/app/endlicherautomat/EndlicherState.ts
+++ src/app/endlicherautomat/EndlicherState.ts
@@ -1,4 +1,3 @@
-import { MatDialog, MatDialogRef } from "@angular/material/dialog";
 import { State } from "../../../statemachine/src/lib/statemachine/state";
 import { Transition } from "../../../statemachine/src/lib/statemachine/stateconnections/Transition";
 import { EndlicheTransition } from "./EndlicheTransition";
@@ -6,9 +5,6 @@ import { EndlicheTransition } from "./EndlicheTransition";
 export class EndlicherState extends State {
     override makeTransition(destination: State): Transition {
         return new EndlicheTransition(this, destination);
-    }
-    override openEditDialog(dialog: MatDialog): MatDialogRef<any, any> {
-        throw new Error("Method not implemented.");
     }
     override isDeterministic(): boolean {
         return true;
