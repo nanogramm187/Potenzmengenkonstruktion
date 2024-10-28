8c63303f1a7d462f747493cfa322d16425c917dc
nanogramm187 <lara.weinberger@hotmail.com>
2024-10-28 10:30:58 +0100

    tutorial-dialog

---
+++ src/app/tutorial-dialog/tutorial-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutorial-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './tutorial-dialog.component.html',
+  styleUrl: './tutorial-dialog.component.scss'
+})
+export class TutorialDialogComponent {
+
+}
